c9f5495b7ff5bfd45638f9628e30deea4713f21d
Abhishek Soni <abhisheksoni@vasyerp.com>
2024-07-08 13:51:27 +0530

    Initial Commit

---
+++ ArchitectureMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureMVVM
+//
+//  Created by Janam Soni on 08/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
